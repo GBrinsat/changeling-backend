4a7da0fc4040fdaa3ea65293bcca6ca9c33f662f
Gerrit Brinsat <gerrit.brinsat@gmx.de>
2023-05-16 13:49:48 +0200

    bullshit

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -52,11 +52,6 @@ router.post("/signup", (req, res) => {
 //Post /auth/login - verifies name and password and returns JWT
 
 router.post("/login", (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*")
-    res.setHeader("Access-Control-Allow-Credentials", "true")
-    res.setHeader("Access-Control-Max-Age", "1800")
-    res.setHeader("Access-Control-Allow-Headers", "content-type")
-    res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" )
     const {name, password} = req.body
 
     if(name === "" || password === "") {
